4f43dbf9ab3cbbc6c374efa5e0706d6d29313edc
dantheta <dan.abakah@gmail.com>
2013-07-22 02:50:49 +0000

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #Returns the full title of the page
+  def full_title page_title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.present?
+      return "#{base_title} | #{page_title}"
+    else
+      return base_title
+    end
+  end
+  
 end
